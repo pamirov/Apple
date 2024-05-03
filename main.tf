db1c3bd1116d164f726ca634ef49af6d52791915
Abai <pamirovkr@gmail.com>
2024-05-03 15:49:14 -0500

    test commit

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.48.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
     }
+  }
+
+  backend "s3" {
+    bucket         	   = "apple-terraform-infra"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table = "terraform-lock"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
-
